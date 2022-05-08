4f690024d19be30fbfe4842880836288b46d2192
levay <524515266@qq.com>
2022-05-08 13:44:20 +0800

    create multimodule

---
+++ pro-spring/implementing-controller/src/main/java/com/example/implementingcontroller/ImplementingControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.implementingcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementingControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplementingControllerApplication.class, args);
+    }
+
+}
