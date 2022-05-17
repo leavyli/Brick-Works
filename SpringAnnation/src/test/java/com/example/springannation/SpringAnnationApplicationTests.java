94d12b07f0d7a39bd5613f5c19bc3596c5779fe7
levay <524515266@qq.com>
2022-05-17 10:34:03 +0800

    init

---
+++ SpringAnnation/src/test/java/com/example/springannation/SpringAnnationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springannation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnnationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
