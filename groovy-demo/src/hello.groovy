d8c46df260301e89eea71f298d091619de09f31c
levay <524515266@qq.com>
2022-06-04 16:31:13 +0800

    更改位置

--- groovy-demo/src/hello.groovy
+++
@@ -1,8 +0,0 @@
-class hello {
-    static void main(String[] args) {
-        def x = 4;
-        // Using a simple println statement to print output to the console
-        println('Hello World' + x);
-    }
-}
-
