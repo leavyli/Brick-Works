14400f90b2444d2a907395a2fd109da6a2c3a824
levay <524515266@qq.com>
2022-06-04 16:28:04 +0800

    #gradle-demo hello.groovy  task

---
+++ gradle-demo/app/hello/build.gradle.groovy
@@ -0,0 +1,19 @@
+import javax.swing.DefaultBoundedRangeModel
+import java.awt.print.Printable
+
+task hello {
+    println 'hello task'
+
+}
+//class GreetingPlugin implements Plugin<Project> {
+//    void apply(Project project) {
+//        project.task('hello') {
+//            doLast {
+//                println 'Hello from the GreetingPlugin'
+//            }
+//        }
+//    }
+//}
+//
+//// Apply the plugin
+//apply plugin: GreetingPlugin
---
+++ groovy-demo/src/hello.groovy
@@ -0,0 +1,8 @@
+class hello {
+    static void main(String[] args) {
+        def x = 4;
+        // Using a simple println statement to print output to the console
+        println('Hello World' + x);
+    }
+}
+
