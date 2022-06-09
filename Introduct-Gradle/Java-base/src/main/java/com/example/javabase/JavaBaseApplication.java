669f776b13907504b06e2ad4ee371552d8990179
levay <524515266@qq.com>
2022-06-09 15:45:44 +0800

    Java-base

---
+++ Introduct-Gradle/Java-base/src/main/java/com/example/javabase/JavaBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaseApplication.class, args);
+    }
+
+}
---
+++ Introduct-Gradle/Java-base/src/test/java/com/example/javabase/JavaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
