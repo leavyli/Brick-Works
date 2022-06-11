3c7c0a17dea20dc0407efc648d5642de9078e877
levay <524515266@qq.com>
2022-06-11 18:31:13 +0800

    DoJoinPoint

---
+++ Java-design-pattern/src/main/java/com/example/javadesignpattern/facePattern/aop/DoJoinPoint.java
@@ -0,0 +1,13 @@
+package com.example.javadesignpattern.facePattern.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author saino
+ * LastModify 21:38
+ */
+@Aspect
+@Component
+public class DoJoinPoint {
+}
