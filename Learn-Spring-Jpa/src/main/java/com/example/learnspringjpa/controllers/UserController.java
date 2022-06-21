0f2cf1d8667e7e6f7996b442c581ddcf670e7962
levay <524515266@qq.com>
2022-06-21 09:52:17 +0800

    分页和排序使用

--- Learn-Spring-Jpa/src/main/java/com/example/learnspringjpa/controllers/UserController.java
+++ Learn-Spring-Jpa/src/main/java/com/example/learnspringjpa/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.example.learnspringjpa.controllers;
 import com.example.learnspringjpa.entities.User;
 import com.example.learnspringjpa.repository.UserRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +14,17 @@ import org.springframework.web.bind.annotation.*;
  * LastModify 6:27
  */
 @RestController
-@RequestMapping(path="/api/v1")
+@RequestMapping(path = "/api/v1")
 public class UserController {
     private final UserRepository userRepository;
+
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     /**
      * 保存用户
+     *
      * @param user 用户
      * @return 保存后的用户
      */
@@ -32,12 +36,22 @@ public class UserController {
 
     /**
      * 根据分页信息查找用户
+     *
      * @param request 分页信息
      * @return 用户列表
      */
-    @GetMapping(path="users")
-    @ResponseBody
+    @GetMapping(path = "users")
     public Page<User> getAllUsers(Pageable request) {
         return userRepository.findAll(request);
+    }
+
+    @GetMapping(path = "page")
+    public Page<User> getAllUsersByPage() {
+        return userRepository.findAll(PageRequest.of(0, 20, Sort.by(new Sort.Order(Sort.Direction.ASC, "name"))));
+    }
+
+    @GetMapping(path = "/sort")
+    public Iterable<User> getAllUsersBySort() {
+        return userRepository.findAll(Sort.by(new Sort.Order(Sort.Direction.ASC, "name")));
     }
 }
