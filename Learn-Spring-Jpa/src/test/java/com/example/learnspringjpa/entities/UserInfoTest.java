011de5947d2dc35edc185eb2f784711712a04e13
levay <524515266@qq.com>
2022-06-26 13:11:24 +0800

    redis配置成功

--- Learn-Spring-Jpa/src/test/java/com/example/learnspringjpa/entities/UserInfoTest.java
+++ Learn-Spring-Jpa/src/test/java/com/example/learnspringjpa/entities/UserInfoTest.java
@@ -5,6 +5,7 @@ import com.example.learnspringjpa.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,11 +16,20 @@ class UserInfoTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Test
     void testUserInfoOneToOne() {
         User user = userRepository.save(User.builder().name("saino").email("kk@google.com").build());
         UserInfo userInfo = userInfoRepository.save(UserInfo.builder().age(20).school("school").user(user).build());
         assertEquals(userInfo.getUser(), user);
+
+    }
+
+    @Test
+    void testRedis() {
+        redisTemplate.opsForValue().set("key", "value");
 
     }
 
