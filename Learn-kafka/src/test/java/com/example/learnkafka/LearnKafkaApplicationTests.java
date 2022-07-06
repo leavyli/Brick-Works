084549956e217193ba50a595e31b51ab3520f1c8
levay <524515266@qq.com>
2022-07-06 20:40:06 +0800

    learn kafka

--- Learn-kafka/src/test/java/com/example/learnkafka/LearnKafkaApplicationTests.java
+++ Learn-kafka/src/test/java/com/example/learnkafka/LearnKafkaApplicationTests.java
@@ -3,11 +3,23 @@ package com.example.learnkafka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 class LearnKafkaApplicationTests {
 
+    static void config(Map<String, ?> map) {
+    }
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testkk() {
+        Map<String, Long> map = new HashMap<>();
+        map.put("a", 1L);
     }
 
 }
