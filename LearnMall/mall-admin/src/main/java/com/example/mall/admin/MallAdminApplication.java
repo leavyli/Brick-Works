57f586a65757b08ec4ff77c24123dcc788e3cfae
levay <524515266@qq.com>
2022-07-28 05:49:22 +0800

    添加模块mall-mbg

---
+++ LearnMall/mall-admin/src/main/java/com/example/mall/admin/MallAdminApplication.java
@@ -0,0 +1,15 @@
+package com.example.mall.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author saino
+ * LastModify 15:18
+ */
+@SpringBootApplication
+public class MallAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
+}
