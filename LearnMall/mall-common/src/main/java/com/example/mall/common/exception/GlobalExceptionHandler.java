6a5116c4c2dcef46c85568aae234236d69e0e65c
levay <524515266@qq.com>
2022-08-12 20:19:49 +0800

    定义全局异常处理

---
+++ LearnMall/mall-common/src/main/java/com/example/mall/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.example.mall.common.exception;
+
+import com.example.mall.common.api.CommonResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Author saino
+ * LastModify 19:39
+ * 全局异常处理类
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public  CommonResult<Object> handleValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String message = null;
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
+            if (fieldError != null) {
+                message = fieldError.getField() + fieldError.getDefaultMessage();
+            }
+        }
+        return CommonResult.validateFailed(message);
+    }
+
+}
