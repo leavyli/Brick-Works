860b122024ef3752e87dd98afd57d17cdb8b0bbf
levay <524515266@qq.com>
2022-09-02 11:19:11 +0800

    添加xml文件

--- LearnMall/mall-mbg/src/main/java/com/example/mall/mbg/mapper/Admin/AdminRoleRelationMapper.java
+++ LearnMall/mall-mbg/src/main/java/com/example/mall/mbg/mapper/Admin/AdminRoleRelationMapper.java
@@ -2,11 +2,14 @@ package com.example.mall.mbg.mapper.Admin;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.mall.mbg.model.Admin.AdminRoleRelation;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Author saino
  * Create 11:08
  */
+@Mapper
 public interface AdminRoleRelationMapper extends BaseMapper<AdminRoleRelation> {
 
 }
--- LearnMall/mall-mbg/src/main/java/com/example/mall/mbg/mapper/Admin/RoleResourceRelationMapper.java
+++ LearnMall/mall-mbg/src/main/java/com/example/mall/mbg/mapper/Admin/RoleResourceRelationMapper.java
@@ -2,10 +2,12 @@ package com.example.mall.mbg.mapper.Admin;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.mall.mbg.model.Admin.RoleResourceRelation;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * Author saino
  * Create 15:13
  */
+@Mapper
 public interface RoleResourceRelationMapper extends BaseMapper<RoleResourceRelation> {
 }
