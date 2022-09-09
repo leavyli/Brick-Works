8ba5c70d54a1bc60281b231bb937c23afb15b939
levay <524515266@qq.com>
2022-09-09 16:03:24 +0800

    #添加search模块

---
+++ LearnMall/mall-search/src/main/java/com/example/mall/search/MallSearchApplication.java
@@ -0,0 +1,15 @@
+package com.example.mall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author saino
+ * LastModify 15:18
+ */
+@SpringBootApplication
+public class MallSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallSearchApplication.class, args);
+    }
+}
