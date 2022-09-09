1b7682648684dee83d09bf4857dad2ccc2589c4f
levay <524515266@qq.com>
2022-09-09 17:24:35 +0800

    #跳过测试的打包成功

---
+++ LearnMall/mall-common/src/main/java/com/example/mall/common/MallCommonApplication.java
@@ -0,0 +1,15 @@
+package com.example.mall.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author saino
+ * LastModify 15:18
+ */
+@SpringBootApplication
+public class MallCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallCommonApplication.class, args);
+    }
+}
---
+++ LearnMall/mall-mbg/src/main/java/com/example/mall/mbg/MallMbgApplication.java
@@ -0,0 +1,15 @@
+package com.example.mall.mbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author saino
+ * LastModify 15:18
+ */
+@SpringBootApplication
+public class MallMbgApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallMbgApplication.class, args);
+    }
+}
