803efe38c00ef249552ab9d6c4bb9c980663d1c4
levay <524515266@qq.com>
2022-09-09 17:34:07 +0800

    #admin测试的打包成功

--- LearnMall/mall-admin/src/test/java/com/example/mall/admin/service/impl/AdminServiceImplTest.java
+++ LearnMall/mall-admin/src/test/java/com/example/mall/admin/service/impl/AdminServiceImplTest.java
@@ -6,8 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Slf4j
 class AdminServiceImplTest {
     @Autowired
