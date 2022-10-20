83c83626331fbda5ec42d67764582d860d6d303b
levay <524515266@qq.com>
2022-10-20 13:05:06 +0800

    init

--- src/main/java/com/example/learnmybatis/LearnMybatisApplication.java
+++ src/main/java/com/example/learnmybatis/LearnMybatisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnMybatisApplication {
+public class LearnMyBatisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnMybatisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearnMyBatisApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/learnmybatis/LearnMybatisApplicationTests.java
+++ src/test/java/com/example/learnmybatis/LearnMybatisApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LearnMybatisApplicationTests {
+class LearnMyBatisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
