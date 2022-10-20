7185eb6735b3d216f5eb6e4773d1a069bce2b9e4
levay <524515266@qq.com>
2022-10-20 12:59:45 +0800

    remove ReadMe.md

---
+++ src/main/java/com/example/learnmybatis/LearnMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learnmybatis/LearnMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
